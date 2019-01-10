30b64f335911652f3cba833a1983c021540185c5
Ahn, Seongjin <jam45@naver.com>
2019-01-10 07:12:51 +0000

    add tridiag-common.hh

---
+++ include/tridiag-common.hh
@@ -0,0 +1,12 @@
+#ifndef _TRIDIAG_COMMON_HH_
+#define _TRIDIAG_COMMON_HH_
+
+#define NUM_OF_ARRAY_IN_TRIDIAGS 3
+
+enum index_name { 
+  i_ld, // an index for lower offdiagonal array
+  i_ud, // an index for upper offdiagonal array
+  i_d, // an index for diagonal array
+};
+
+#endif // _TRIDIAG_COMMON_HH_
