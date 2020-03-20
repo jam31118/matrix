5e3a33fa9349744b16b154a8b01c9ce20bfbba00
Ahn, Seongjin <jam45@naver.com>
2020-03-21 00:01:04 +0900

    add explicit instantiation for some tridiag routines

--- src/matrix.cc
+++ src/matrix.cc
@@ -1,1 +1,16 @@
 #include "../include/matrix.hh"
+
+#include <complex>
+
+template int tridiag_mul_forward<double, double>(
+		double *, double *, double *, 
+		double *, double *, long);
+
+template int tridiag_mul_forward<double, std::complex<double>>(
+		double *, double *, double *,
+		std::complex<double> *, std::complex<double> *, long);
+
+template int tridiag_mul_forward<std::complex<double>, std::complex<double>>(
+		std::complex<double> *, std::complex<double> *, std::complex<double> *,
+		std::complex<double> *, std::complex<double> *, long);
+
